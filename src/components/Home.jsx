0e73c40485b681d2a1f77e73748e678ffbdbc5f6
oigoga <gogaelisabeth21@gmail.com>
2023-03-10 10:56:55 +0100

    hme

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Postskeleton from "./Postskeleton";
+
+const Home = ({ posts }) => {
+  return (
+    <>
+      {posts.map((post) => (
+        <div>
+          <Postskeleton key={post.id} post={post} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Home;
